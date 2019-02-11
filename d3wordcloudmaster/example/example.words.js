58d25d0129fe4fa16e0cffa8a2139d8ad8921a05
sai krishna <ssydev05@gmail.com>
2019-02-11 15:33:29 -0500

    Add files via upload

---
+++ d3wordcloudmaster/example/example.words.js
@@ -0,0 +1,9 @@
+var words = [
+{text: 'William', size: 5, href: "http://omgili.com/ri/.wHSUbtEfZTFf1WE5pGijT_s7Q_LC9825.ypzUJ0rhYyOrB_sIpX69D7o9K8F7pp9_OCFrzo9xIofAhVWKWyd_nyKNL4M7dKydyXuxzIZTs-"},
+{text: 'Seward', size: 5, href: "http://omgili.com/ri/t1o2C2H78UdfkCmqpg8jbkX0qIpLxLRXoT.LMU_4p8Evk.Qa.WcsL2O5CifkWWL4w4tJgQZFF5Y3yxzm0xHWmiXMBd7BSDjNpulKmZmzud6IgwXZjMi6mg--"},
+{text: 'North', size: 5, href: "http://omgili.com/ri/.wHSUbtEfZRFnyDYmf.VdM.hvIQX5xeQw5zAzPS.qBEfaJBnSUQdRvNpcHLTcsRH.aR3yvXbGlKMdontpfyxbRb56OZD.fDlnB2vr10OhJURhjJiBudcmnODWPk.vYjBCu1xQYfhomEuynam98_rBzV8sbwyCGvy"},
+{text: 'South', size: 5, href: "http://omgili.com/ri/.wHSUbtEfZRFnyDYmf.VdM.hvIQX5xeQw5zAzPS.qBHIpNM4qwi_ha86hIR63FHX_OFcsk2OkoZFwQCIqd2L_ZpaVLz1VGx4pdPYXf1rSvWM9gu8Acf5sPq11_2TV_p6jvw_vnvoWqolYhNGgOv3fg--"},
+{text: 'Trump', size: 10, href: "http://omgili.com/ri/.wHSUbtEfZSPfVRrC83GDKJbKDRbkYjxGYNauX4aDbl77ewl3nXYa3.Tx7oz4W.Jt3k12JeKXMU8cJcx6r3xcFLnXS6bAehVy7HNov7xRho-"},
+{text: 'GOP', size: 5, href: "http://omgili.com/ri/jHIAmI4hxg.08s7MHOUpOiM9vbZEfMb2t7LfUefz8ByUU2rTNtbtO01m3IEdbzuj.p805a82CqMygAtkHu4Bj5017bQaWhtje.vYwochA_qEU5r46X8IWQzZsXK3e9g7x6xL9FZT6nypNO6Q7OpcqQ--"},
+{text: 'Hillary', size: 5, href: "http://omgili.com/ri/t1o2C2H78Uc0EsQYnMlDMverl9.z9oCuZxLs3tz_MKPnlzGZdObrD.UJZOac2HPEg9Nwh3bia5KripPwQEs0cg--"},
+];
